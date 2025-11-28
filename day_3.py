4518a94164f71e2aa03aa301b67b8e56838ce549
Stranger4uu <yashsaini4824@gmail.com>
2025-11-29 02:48:20 +0530

    Added Automatic operator

--- day_3.py
+++ day_3.py
@@ -1,6 +1,6 @@
 print("Hey..Welcome to the rollercoaster")
 height = int(input("Enter your height in cm : "))
-if height > 120:
+if height >= 120:
     print("Congo...  You can ride")
 else:
     print("You cannot ride because your height is less than 120 cm sorry :( ")
