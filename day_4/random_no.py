51067cea081911fe8bf3ca0814fde53d874f3d83
Stranger4uu <yashsaini4824@gmail.com>
2025-12-04 21:16:26 +0530

    added new files

---
+++ day_4/random_no.py
@@ -0,0 +1,18 @@
+# used to generate any 2 int random number 
+import random
+
+random_1 = random.randint(1,10)
+print(random_1)
+
+
+#to generate random number between 0-1
+random_2 = random.random() # we can also multiply it by any number like , 2,5,19,4 etc
+print(random_2)
+
+
+
+# used to generate any 2 float random number 
+
+random_3 = random.uniform(1,10)
+print(random_3)
+
