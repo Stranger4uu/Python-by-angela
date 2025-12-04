20ffc08f75f1be4899c009f32c6173c2e5557f8b
Stranger4uu <yashsaini4824@gmail.com>
2025-12-04 23:19:30 +0530

    added random_toss program in random_no file

--- day_4/random_no.py
+++ day_4/random_no.py
@@ -16,3 +16,16 @@ print(random_2)
 random_3 = random.uniform(1,10)
 print(random_3)
 
+#program of random toss 
+
+
+random_toss = random.randint(0, 1)   # 0 = head, 1 = tail
+guess = input("Enter your guess (Head/Tail): ").lower()
+
+result = "head" if random_toss == 0 else "tail"
+print("Toss result:", result.capitalize())
+
+if guess == result:
+    print("You guessed it right!")
+else:
+    print("Sorry, you guessed it wrong :(")
