434080c5d1f820b2c9dffb9b6611842c96b4e006
Stranger4uu <yashsaini4824@gmail.com>
2025-12-07 15:17:43 +0530

    added day 5 folder and updated read me

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python-by-angela
 will be uploding my python codes here :)
+
+# there is no day 1 and day 2 because that were basics of python and i have practised those basic codes in file named "basic.py"
---
+++ day_5/sum_of_list.py
@@ -0,0 +1,11 @@
+students_score = [30,94,53,53,66,53,63,72,24,9,3,23,44,16,86,84,57,98,66,99,87,45,88,13,27,47]
+sum_of_all_students_score = sum(students_score)
+print(sum_of_all_students_score)
+
+# Or
+
+sum = 0
+for scores in students_score:
+    sum += scores
+
+print(sum)
