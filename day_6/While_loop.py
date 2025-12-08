6c13838a46d606efbe1ad6b3f09e001f1a3e4f11
Stranger4uu <yashsaini4824@gmail.com>
2025-12-08 15:34:38 +0530

    commented out while loop file because will be doing more while program practice tommorow

--- day_6/While_loop.py
+++ day_6/While_loop.py
@@ -1,35 +1,35 @@
-def turn_left():
-    # Define the behavior of turn_left here
-    pass
+# def turn_left():
+#     # Define the behavior of turn_left here
+#     pass
 
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
     
-def turn_around():
-    turn_left()
-    turn_left()
+# def turn_around():
+#     turn_left()
+#     turn_left()
    
-def move():
-    pass
+# def move():
+#     pass
 
-def loop():
-    move()
-    turn_left()
-    move()
-    turn_right()
-    move()
-    turn_right()
-    move()
-    turn_left()
+# def loop():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
 
 
-number_of_hurdles = 6
-while number_of_hurdles > 0:
-    loop()
-    number_of_hurdles -= 1
-    print(number_of_hurdles)
+# number_of_hurdles = 6
+# while number_of_hurdles > 0:
+#     loop()
+#     number_of_hurdles -= 1
+#     print(number_of_hurdles)
 
 
     
