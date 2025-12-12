12f5f70e95a6bf6e2df782ccf529becfb5222abf
Stranger4uu <yashsaini4824@gmail.com>
2025-12-12 18:23:38 +0530

    updated handman game, now it is completed

--- day_7/hangman_game.py
+++ day_7/hangman_game.py
@@ -62,11 +62,11 @@ random_words = [
 
 lives = 6
 print("You have 6 lives and each time u guess wrong your lives will - by 1 ")
-choosen_word = random.choice(random_words)
+chosen_word = random.choice(random_words)
 
 
 placeholder = ""
-word_length = len(choosen_word)
+word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
 
@@ -85,7 +85,7 @@ while not Game_over:  # Fix the condition to use != for the game loop
 
     display = ""
 
-    for letter in choosen_word:
+    for letter in chosen_word:
         if letter == guess:
             display += letter 
             correct_letters.append(letter)
@@ -96,7 +96,7 @@ while not Game_over:  # Fix the condition to use != for the game loop
     print("word to guess: " + display)
     
 
-    if guess not in choosen_word:
+    if guess not in chosen_word:
         lives -=1 
         print(f"You lost a live because : {guess} is not in the word ")
         print(f"Now you have : ******** {lives} ******** lives ")
@@ -105,7 +105,7 @@ while not Game_over:  # Fix the condition to use != for the game loop
         
     if lives == 0:
         print("Game over! You have ******** 0 ******** lives left.")
-        print(f"The word was : {choosen_word}")
+        print(f"The word was : {chosen_word}")
         Game_over = True
         break
 
@@ -114,4 +114,4 @@ while not Game_over:  # Fix the condition to use != for the game loop
     if "_" not in display:
         Game_over = True
         print("******** You won..... ********")
-        print(f"Correct the word is : {choosen_word}")
+        print(f"Correct the word is : {chosen_word}")
