9ddb77c04d7dc5d0ba242de0e0390802fadf03f1
Stranger4uu <yashsaini4824@gmail.com>
2025-12-20 08:58:17 +0530

    added 2 new files

---
+++ day_8/Input_Functions.py
@@ -0,0 +1,39 @@
+# function without input
+
+# def greet():
+#     print("HEY")
+#     print("HELLO")
+#     print("HI")
+    
+# greet()
+
+
+# def greet_with_name(name):
+#     print(f"Hey {name}")
+#     print(f"How it's going {name}")
+
+# greet_with_name("Yash")
+
+# #  here name = Parameter and "Yash" = Argument
+
+
+
+# def life_in_weeks():
+#     current_age = int(input("What is your current age in years ? : "))
+#     user_age_in_weeks = (current_age * 365)/7
+#     weeks_left = 4693 - user_age_in_weeks
+#     print(f"You have {weeks_left} weeks left.")
+    
+    
+    
+# life_in_weeks()
+
+
+# def greet_with(name, location):
+#     print(f"Heyy {name}")
+#     print(f"What is it in {location}")
+
+# greet_with("Yash","Alwar") 
+
+
+
