3927928dc0ec2e02c4449313a4cbe99712c722a2
Stranger4uu <yashsaini4824@gmail.com>
2025-12-25 16:44:34 +0530

    created new day 9 folder and also created new dictionaries file in that folder , will be doing more codes from different device

---
+++ day_9/dictionaries.py
@@ -0,0 +1,31 @@
+# to create a new dictionary
+
+python_dictionary = {
+    "loop" : "The action of doing something again and again",
+    "Bug" : "An error in a program that prevents the program from running as excepted"
+}
+
+# # add a new dictionary later
+# python_dictionary["Name"] = "My name is yash"
+
+# print(python_dictionary)
+
+# # create a new empty dictionary or vipe the privious one
+
+# python_dictionary =  {}
+
+# print(python_dictionary)
+
+
+# # edit a item in a dictionary
+
+# python_dictionary["Name"] = "My name is nothing"
+
+# print(python_dictionary)
+
+
+# loop through a dictionary
+
+for key in python_dictionary:
+    print(key)
+    print(python_dictionary[key])
