0904587b1db45e822b527c5f191f0d65af1465bc
Stranger4uu <yashsaini4824@gmail.com>
2025-12-30 22:08:37 +0530

    Added 'Thanks for using calculator ' text in the end of the program

--- day_10/calculator_project.py
+++ day_10/calculator_project.py
@@ -53,15 +53,16 @@ def calculator():
         answer = operations[operaton_symbol](num1,num2)
         print(f"{num1} {operaton_symbol} {num2} = {answer}")
 
-        choice = input(f"Type 'y' to continue calculating with {answer} Or Type 'n' to start a new calculation Or Type 'stop' to stop program ").lower()
+        choice = input(f"Type 'y' to continue calculating with {answer} Or Type 'n' to start a new calculation Or Type 'stop' to stop program : ").lower()
         if choice =="stop":
+            print("Thanks for using calculator :p ")
             return 0
         if choice =="y":
             num1 = answer
 
         else:
             should_accumulate = False
-            print("\n"*50)
+            print("\n" * 50)
             calculator()
 
 calculator()
