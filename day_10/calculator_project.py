86d0ac84e3333da213f744c3bf1c3a51c117f4e3
Stranger4uu <yashsaini4824@gmail.com>
2025-12-30 21:59:40 +0530

    completed calculator program

---
+++ day_10/calculator_project.py
@@ -0,0 +1,58 @@
+logo = r"""
+   ┌─────────┐
+   │  CALC   │
+   ├─────────┤
+   │ 7 8 9 + │
+   │ 4 5 6 - │
+   │ 1 2 3 * │
+   │ 0 . = / │
+   └─────────┘
+"""
+
+
+
+
+def add(n1,n2):
+    return n1 + n2
+
+def subtract(n1,n2):
+    return n1 - n2
+
+def multiply(n1,n2):
+    return n1 * n2
+
+def divide(n1,n2):
+    return n1 / n2 
+
+operations = {
+    "+": add ,
+    "-": subtract,
+    "*": multiply,
+    "/": divide 
+}
+
+def calculator():
+    print(logo)
+
+    should_accumulate = True
+    num1 = float(input("what's the first number ? : "))
+
+    while should_accumulate:
+        for symbols in operations:
+            print(symbols)
+        operaton_symbol = input("Pick an operation ? : ")
+        num2 = float(input("What's the next number : "))
+        answer = operations[operaton_symbol](num1,num2)
+        print(f"{num1} {operaton_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer} or Type 'n' to start a new calculation : ").lower()
+
+        if choice =="y":
+            num1 = answer
+
+        else:
+            should_accumulate = False
+            print("\n"*50)
+            calculator()
+
+calculator()
