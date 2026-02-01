7757a83e80bc59087d75ab89ab199f66e1428ca0
Stranger4uu <yashsaini4824@gmail.com>
2026-02-01 18:07:51 +0530

    did some functions practice code in basic py file

--- basic.py
+++ basic.py
@@ -55,3 +55,45 @@ iq = 69
 cgpa = 8.1
 got_placed = True
 print(f"your iq is : {iq} , your cgpa is : {cgpa} and your placement status is : {got_placed}")
+
+# average of 3 numbers
+
+def avg_of_3_no(a,b,c):
+    avg = (a+b+c)/3
+    print(avg)
+    
+
+avg_of_3_no(4,4,8)
+
+cities = ["awlar","jaipur","delhi",]
+def len_of_cities(list):
+    print(len(list))
+    
+    
+len_of_cities(cities)
+
+def elements_of_list(list):
+    for item in list:
+        print(item,end=" ")
+
+elements_of_list(cities)
+print()
+
+# function to print factorial of n number
+def factorial(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+        print(fact)
+        
+factorial(5)
+
+# function to check if the number is odd or even 
+
+def even_or_odd(n):
+    if n%2 == 0:
+        print("EVEN")
+    else:
+        print("ODD")
+
+even_or_odd(4)
